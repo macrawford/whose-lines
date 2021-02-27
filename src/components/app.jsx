8aef0ccd043e8d650d39dae83dc4c87a07ded350
Matthew Crawford <matthew.crawford92@gmail.com>
2021-02-27 15:12:39 -0800

    Set up React app with express server

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>React Working!!!</div>
+    )
+  }
+};
+
+export default App;
